0f01f68bb3bf64de2f02513639bc75343c706879
cloeb <cloeb@purdue.edu>
2024-03-29 15:20:41 -0400

    .

--- bridge.sh
+++ bridge.sh
@@ -1,2 +1,2 @@
 ros2 run ros_gz_bridge parameter_bridge /lidar/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked &
-ros2 run ros_gz_bridge parameter_bridge /world/default/model/nist_0/link/base_link/sensor/imu_sensor/imu@sensor_msgs/msg/Imu@gz.msgs.IMU
+#ros2 run ros_gz_bridge parameter_bridge /world/default/model/nist_0/link/base_link/sensor/imu_sensor/imu@sensor_msgs/msg/Imu@gz.msgs.IMU
